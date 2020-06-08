27d622c8eda9e5b9f01cc5f0f829aaee69c218ca
Dominic Letz <dominic@diode.io>
2020-06-08 12:39:50 +0200

    Changig macos flags for static linking

--- build_static.go
+++ build_static.go
@@ -19,6 +19,6 @@ package openssl
 // #cgo linux windows pkg-config: --static libssl libcrypto
 // #cgo linux CFLAGS: -Wno-deprecated-declarations
 // #cgo darwin CFLAGS: -I/usr/local/opt/openssl/include -Wno-deprecated-declarations
-// #cgo darwin LDFLAGS: -L/usr/local/opt/openssl/lib -lssl -lcrypto
+// #cgo darwin LDFLAGS: -l:/usr/local/opt/openssl/lib/libssl.a -l:/usr/local/opt/openssl/lib/libcrypto.a
 // #cgo windows CFLAGS: -DWIN32_LEAN_AND_MEAN
 import "C"
