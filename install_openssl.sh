dd49aeb11d4353c73650f7b1c88baf580799adf1
Dominic Letz <dominic@diode.io>
2021-05-05 21:56:32 +0200

    ensuring prefix/ssl

--- install_openssl.sh
+++ install_openssl.sh
@@ -4,7 +4,7 @@ export PREFIX=/usr/local/openssl
 
 # install openssl
 echo "Build and install openssl......"
-$SUDO mkdir -p $PREFIX && \
+$SUDO mkdir -p $PREFIX/ssl && \
     wget https://www.openssl.org/source/openssl-$VSN.tar.gz && \
     [ "892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df33fbaee5" = "$(sha256sum openssl-$VSN.tar.gz | cut -d ' ' -f1)" ] && \
     tar xzf openssl-$VSN.tar.gz && \
