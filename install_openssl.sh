12e703743b3a3688c307d1f7001f4f816aacb767
Dominic Letz <dominic@diode.io>
2021-06-08 02:04:28 +0200

    Adding extra arguments to install_openssl.sh

--- install_openssl.sh
+++ install_openssl.sh
@@ -13,7 +13,7 @@ $SUDO mkdir -p $PREFIX/ssl && \
     [ "892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df33fbaee5" = "$(sha256sum openssl-$VSN.tar.gz | cut -d ' ' -f1)" ] && \
     tar xzf openssl-$VSN.tar.gz && \
     cd openssl-$VSN && \
-    ./config no-ssl2 no-ssl3 no-dtls no-dtls1 no-idea no-mdc2 no-rc5 no-zlib --prefix=$PREFIX && \
+    ./config no-ssl2 no-ssl3 no-dtls no-dtls1 no-idea no-mdc2 no-rc5 no-zlib --prefix=$PREFIX "$@" && \
     make depend && make && $SUDO make install_sw install_ssldirs && \
     $SUDO cp -R include/internal $PREFIX/include && \
     $SUDO cp *.h $PREFIX/ && \
